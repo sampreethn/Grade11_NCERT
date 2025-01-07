c12a5f7a4a1b8ceab23adf995f3efc5ab8f768dd
Daredevil <84895353+sampreethn@users.noreply.github.com>
2025-01-07 07:35:49 +0530

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -1,3 +1,5 @@
+#This code is to check if a given item is in the tuple ie if a student is in the attendance list
+#The given question is Write a program to input names of n students and store them in a tuple.Also input a name from the user and find if thisstudent is there in the tuple or not
 num_students=int(input("Enter the number of students :: "))
 students = []
 for i in range(num_students):
